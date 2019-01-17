f6d0999f40f0ce8f3b2b4a9804ed46dfda70d4e8
mightymarty <morgan.mellor@gmail.com>
2019-01-16 22:28:32 -0800

    adding var.

--- terraform/container-app-sample-environment.tf
+++ terraform/container-app-sample-environment.tf
@@ -3,10 +3,10 @@ provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=1.21.0"
 
-  subscription_id = "$ARM_SUBSCRIPTION_ID"
-  client_id       = "$ARM_CLIENT_ID"
-  client_secret   = "$ARM_CLIENT_SECRET"
-  tenant_id       = "$ARM_TENANT_ID"
+  subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
+  client_id       = "${var.ARM_CLIENT_ID}"
+  client_secret   = "${var.ARM_CLIENT_SECRET}"
+  tenant_id       = "${var.ARM_TENANT_ID}"
 }
 
 # Create a resource group
