87188db5eb90fb1fda32db06c464552e10ca4099
mightymarty <morgan.mellor@gmail.com>
2019-01-16 22:57:57 -0800

    hardcode tennant id

--- terraform/container-app-sample-environment.tf
+++ terraform/container-app-sample-environment.tf
@@ -11,7 +11,7 @@ provider "azurerm" {
   subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
   client_id       = "${var.ARM_CLIENT_ID}"
   client_secret   = "${var.ARM_CLIENT_SECRET}"
-  tenant_id       = "${var.ARM_TENANT_ID}"
+  tenant_id       = "2076a2b7-809d-4f17-b222-d88effb0cefb"
 }
 
 # Create a resource group
